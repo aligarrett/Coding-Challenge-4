2ca9d0f6175c862416bdc8321a327b78acb3bef4
aligarrett <aliyah.garrett29@gmail.com>
2024-09-11 14:10:53 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,36 @@
+// Task 1: Create an Inventory Array of Product Objects
+let Inventory = [
+    {
+        name: "Necklace",
+        price: 250.00,
+        quantity: 30,
+        lowStockLevel: 10
+    },
+    {
+        name: "Ring",
+        price: 50.00,
+        quantity: 25,
+        lowStockLevel: 10
+    },
+    {
+        name: "Earrings",
+        price: 100.00,
+        quantity: 20,
+        lowStockLevel: 10
+    },
+    {
+        name: "Bracelet",
+        price: 125.00,
+        quantity: 15,
+        lowStockLevel: 5
+    },
+    {
+        name: "Anklet",
+        price: 75.00,
+        quantity: 10,
+        lowStockLevel: 5
+    },
+]
+
+// Results
+console.log(Inventory);
